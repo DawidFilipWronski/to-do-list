eecbd55a3b00530a063356191d1b2fd7d7b998cc
Dawid Wroński <dawid.filip.wronski@gmail.com>
2022-06-12 17:07:18 +0200

    add edit task possibility

--- script.js
+++ script.js
@@ -2,24 +2,38 @@ const taskInput = document.querySelector('.task');
 const addTaskButton = document.querySelector('.addTask');
 const taskList = document.querySelector('.taskList');
 
-
 function addTask() {
     
     const deleteButton = createDomElement('button', 'x');
     const taskContainer = createDomElement('div', '');
     const task = createDomElement('p', getTaskContent() );
+    const editTaskInput = createDomElement('input', '');
+    editTaskInput.classList = 'hide';
 
     addDomElement( taskContainer, taskList);
     addDomElement( task, taskContainer);
-    addDomElement( deleteButton, taskContainer);
-    returnData();    
-
+    addDomElement( deleteButton, taskContainer);       
+    //remove task 
     deleteButton.addEventListener('click', removeTask);
-    taskContainer.addEventListener('click', editTask);
+    //click task and toggle p with input to edit task
+    task.addEventListener('click', function editTaskMode () {
+        task.classList.toggle('hide');
+        editTaskInput.classList.toggle('hide');        
+        addDomElement( editTaskInput, taskContainer);
+        editTaskInput.value = task.textContent;
+        editTaskInput.focus();                    
+    });
+    // toggle input with p to stop edit task value
+    editTaskInput.addEventListener('focusout', editTask);    
+
+    function editTask() {
+        editTaskInput.classList.toggle('hide');
+        task.classList.toggle('hide');
+        task.textContent = editTaskInput.value;        
+    }
+    //clear input after add new task to prep for next one
     clearValue(taskInput);
 }
-
-
 
 function getTaskContent () {
     let taskContent = taskInput.value;
@@ -38,13 +52,10 @@ function createDomElement (type, content) {
 function removeTask() {
     this.parentNode.remove();
 }
+
 function clearValue(element) {
     element.value = '';
 }
-function returnData (dataToReturn) {
-    return dataToReturn;
-}
-
 
 addTaskButton.addEventListener('click', addTask);
 taskInput.addEventListener('keydown', (e) =>{
