4adf2a3f947e59872941836b7fdb8ab443fd48f9
Dawid Wroński <dawid.filip.wronski@gmail.com>
2022-06-12 15:44:24 +0200

    add functionalities- add and remove task

---
+++ script.js
@@ -0,0 +1,53 @@
+const taskInput = document.querySelector('.task');
+const addTaskButton = document.querySelector('.addTask');
+const taskList = document.querySelector('.taskList');
+
+
+function addTask() {
+    
+    const deleteButton = createDomElement('button', 'x');
+    const taskContainer = createDomElement('div', '');
+    const task = createDomElement('p', getTaskContent() );
+
+    addDomElement( taskContainer, taskList);
+    addDomElement( task, taskContainer);
+    addDomElement( deleteButton, taskContainer);    
+
+    deleteButton.addEventListener('click', removeTask);
+    taskContainer.addEventListener('click', editTask);
+    clearValue(taskInput);
+}
+
+
+
+function getTaskContent () {
+    let taskContent = taskInput.value;
+    return taskContent;
+}
+
+function addDomElement (element, destination) {
+    destination.appendChild(element);
+}
+
+function createDomElement (type, content) {
+    let name = document.createElement(type);
+    name.textContent = content;
+    return name;
+}
+function removeTask() {
+    this.parentNode.remove();
+}
+function clearValue(element) {
+    element.value = '';
+}
+function returnData (dataToReturn) {
+    return dataToReturn;
+}
+
+
+addTaskButton.addEventListener('click', addTask);
+taskInput.addEventListener('keydown', (e) =>{
+    e.code == 'Enter'? addTask() : false;   
+})
+
+
